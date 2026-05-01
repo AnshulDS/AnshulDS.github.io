742bf84281208775141196dd18a0ac69a2356d00
Anshul Choudhary <anshuljaat471@gmail.com>
2026-05-01 22:34:53 +0530

    Add files via upload

---
+++ tailwind.config.ts
@@ -0,0 +1,18 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        background: "#121212", // Match your video frame sequence bg color here!
+      },
+    },
+  },
+  plugins: [],
+};
+export default config;
